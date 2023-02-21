0a5c6b064d5d5e315f68c64ad75f67ce9228c41a
Galiia-GR <hope05590@gmail.com>
2023-02-21 11:19:06 +0600

    add result

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,30 @@
 class GuessingGame {
-    constructor() {}
+  constructor() {
+    this.minNum = 0;
+    this.maxNum = 0;
+    this.middleNum = 0;
+  }
 
-    setRange(min, max) {
+  setRange(min, max) {
+    this.minNum = min;
+    this.maxNum = max;
 
-    }
+    console.log(min, max);
+  }
 
-    guess() {
+  guess() {
+    this.middleNum = Math.ceil((this.minNum + this.maxNum) / 2);
+    console.log(this.middleNum);
+    return this.middleNum;
+  }
 
-    }
+  lower() {
+    this.maxNum = this.middleNum;
+  }
 
-    lower() {
-
-    }
-
-    greater() {
-
-    }
+  greater() {
+    this.minNum = this.middleNum;
+  }
 }
 
 module.exports = GuessingGame;
